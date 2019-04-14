d272a0562ef094d3ee8d53e79e80866cc24d7193
johanveenpere <johan.veenpere@gmail.com>
2019-04-15 01:27:24 +0300

    uuendused

---
+++ app/src/main/java/cybernet/lunchapp/local/lunchapp/GetProfile.java
@@ -0,0 +1,7 @@
+package cybernet.lunchapp.local.lunchapp;
+
+import android.app.Activity;
+
+public class GetProfile extends updateUI {
+    public GetProfile(Activity activity){super(activity);}
+}
---
+++ app/src/main/java/cybernet/lunchapp/local/lunchapp/getLunches.java
@@ -0,0 +1,14 @@
+package cybernet.lunchapp.local.lunchapp;
+
+import android.app.Activity;
+
+import org.json.JSONArray;
+
+public class getLunches extends updateUI {
+
+    public getLunches(Activity activity){super(activity);}
+
+    protected void onPostExecute(JSONArray result){
+
+    }
+}
